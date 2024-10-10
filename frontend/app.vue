3c0871edd17adb358073fb6b9db78a070ae7b0ca
afeyisa <kenenisafeyisa@gmail.com>
2024-10-10 12:42:18 +0300

    front end setup

---
+++ frontend/app.vue
@@ -0,0 +1,12 @@
+<!-- <template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template> -->
+
+<template>
+  <h1 class="text-3xl font-bold italic bg-slate-300">
+    Hello world!
+  </h1>
+</template>
