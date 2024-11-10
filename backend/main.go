72953a9b625758b8f28b16d9d24a577b6e207ea9
afeyisa <kenenisafeyisa@gmail.com>
2024-11-10 19:49:53 +0300

    added image server end point

--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
-
+	// "encoding/base64"
 	"fmt"
+	// "local-event-management-backend/models/images"
+	// "local-event-management-backend/types"
 	"log"
 	"net/http"
+	// "os"
+	// "strings"
 
 	"local-event-management-backend/handlers"
+
 	"github.com/joho/godotenv"
 )
 
@@ -25,9 +30,11 @@ func main() {
   	mux.HandleFunc("/auth",handlers.HasuraAuthHandler)
 	mux.HandleFunc("/logout", handlers.LogOutHandler)
 	mux.HandleFunc("/protectpageroute",handlers.ProtectPageRoute)
-	mux.Handle("/uploads/", http.StripPrefix("/uploads/", http.FileServer(http.Dir("./uploads/"))))
-	mux.HandleFunc("/upload", handlers.UploadImage)
+	// mux.Handle("/uploads/", http.StripPrefix("/uploads/", http.FileServer(http.Dir("./uploads/"))))
+	mux.HandleFunc("/upload", handlers.ImageUploads)
 	mux.HandleFunc("/tickectcheckout", handlers.Ticketcheckout)
+	mux.HandleFunc("/serveimage", handlers.ServeImage)
+
 	fmt.Println("server started")
 	err = http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
