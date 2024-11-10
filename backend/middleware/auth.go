0770df3aa1a476ecdf09fd3ed538bc6fe8fdecff
afeyisa <kenenisafeyisa@gmail.com>
2024-11-10 22:18:44 +0300

    add auth middleware

---
+++ backend/middleware/auth.go
@@ -0,0 +1,21 @@
+package middleware
+
+import (
+	"net/http"
+	"os"
+)
+
+func Auth(next http.Handler) http.Handler{
+
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			header := r.Header.Get("X-ACTION-SECRET")
+			expectedSecret := os.Getenv("ACTION_SECRET")
+			if expectedSecret != header{
+				http.Error(w, "Unauthorized action", http.StatusUnauthorized)
+				return
+			}
+			next.ServeHTTP(w,r)
+		},
+	)
+}
